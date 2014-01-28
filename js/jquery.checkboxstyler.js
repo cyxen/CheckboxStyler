10e65253a2fe56617729f4245a7f809f83591501
cyxen <suhenko@aisol.ru>
2014-01-28 17:57:34 +0400

    bugfix

--- js/jquery.checkboxstyler.js
+++ js/jquery.checkboxstyler.js
@@ -1,37 +1,45 @@
+
 /*
- * Checkboxstyler plugin for jQuery
- * ---
- * Copyright 2013, Sergey Sukhenko
- * Released under the MIT, BSD, and GPL Licenses.
- */
-(function($) {
-    $.fn.checkboxstyler = function() {
-        //создаем элементы
-        count = 0;
+* Checkboxstyler plugin for jQuery
+* ---
+* Copyright 2013, Sergey Sukhenko
+* Released under the MIT, BSD, and GPL Licenses.
+*/
+(function($){
+	$.fn.checkboxstyler = function(options) {
+     var settings = $.extend({
+        'class':''
+    }, options);
+     this_class = ' ' + settings['class'] + '';
+     if ($('.'+$(this).attr('class')).length>0){
         this.each(function() {
-            $(this).hide();
-            count++;
-            $(this).addClass('checkboxelem_' + count);
-            if ($(this).prop('checked') === false) {
-                $(this).after('<div id="checkboxstyler_' + count + '" class="checkboxstyler noselect"></div>');
-            } else {
-                $(this).after('<div id="checkboxstyler_' + count + '" class="checkboxstyler yesselect"></div>');
-            }
-        });
-        // обрабатываем клики
-        var one_checkbox = $('.checkboxstyler');
-        one_checkbox.click(function(e) {
+           var id_elem=$(this).attr('id');
+           $(this).hide();
+
+           $(this).addClass('checkboxelem_'+id_elem);
+           if ($(this).prop('checked')===false){
+              $(this).after('<div id="checkboxstyler_'+id_elem+'" class="'+this_class+' checkboxstyler noselect"></div>');
+          }
+          else{
+              $(this).after('<div id="checkboxstyler_'+id_elem+'" class="'+this_class+' checkboxstyler yesselect"></div>');
+          }
+          var one_elem=$('#checkboxstyler_'+id_elem)
+          one_elem.click(function(e){
             e.preventDefault();
-            id_array = $(this).attr('id').split('_');
-            if ($(this).hasClass('noselect')) {
+            var temp = $(this).attr('id');
+            temp = temp.replace(/checkboxstyler_/g, "");
+            if ($(this).hasClass('noselect')){
                 $(this).removeClass('noselect');
                 $(this).addClass('yesselect');
-                $('.checkboxelem_' + id_array[1]).attr("checked", true);
-            } else {
+                $('.checkboxelem_'+temp).attr("checked",true);
+            }
+            else{
                 $(this).removeClass('yesselect');
-                $(this).addClass('noselect');
-                $('.checkboxelem_' + id_array[1]).removeAttr("checked");
+                $(this).addClass('noselect'); 
+                $('.checkboxelem_'+temp).removeAttr("checked");
             }
         });
-    };
-})(jQuery);
+      });
+}
+};
+})( jQuery );
